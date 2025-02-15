09a00467bc735b123d5e02285f2a1409e49db4fb
rtk-rnjn <ritik0ranjan+github@gmail.com>
2025-02-15 17:24:20 +0530

    Refactor user route to improve error handling and response structure

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from bson import ObjectId
 from fastapi import APIRouter, HTTPException, Request
 from pydantic import BaseModel, EmailStr
@@ -7,6 +9,7 @@ from pymongo.results import InsertOneResult
 
 from src.app import app, mongo_client
 from src.models import User
+from type import UpdateResult
 
 __all__ = ("create_user", "update_user", "fetch_user")
 
@@ -21,6 +24,7 @@ class CreateResponse(BaseModel):
 class UpdateResponse(BaseModel):
     success: bool
     modified_count: int
+    detail: Optional[str] = None
 
 
 async def _fetch_user(*, email: str, password: str) -> User:
@@ -47,10 +51,12 @@ async def create_user(request: Request, data: User) -> CreateResponse:
     Create a new user and return the inserted ID.
     """
     if await user_exists(email=data.email_address):
-        raise HTTPException(status_code=400, detail="User already exists")
+        return CreateResponse(success=False, inserted_id="")
 
     collection = mongo_client["MomCare"]["users"]
     sendable_data = data.model_dump(mode="json")
+    sendable_data["_id"] = sendable_data.pop("id")
+
     result: InsertOneResult = await collection.insert_one(sendable_data)
 
     response = {"success": True, "inserted_id": str(result.inserted_id)}
@@ -79,7 +85,7 @@ async def fetch_user_by_id(request: Request, _id: str) -> User:
     Fetch user details using the user ID.
     """
     collection = mongo_client["MomCare"]["users"]
-    user = await collection.find_one({"_id": ObjectId(_id)})
+    user = await collection.find_one({"_id": _id})
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
@@ -103,16 +109,17 @@ async def update_user(request: Request, user: User) -> UpdateResponse:
     try:
         user_dumped = user.model_dump(mode="json")
 
-        result = await collection.update_one(
-            {"_id": ObjectId(user.id)},
+        result: UpdateResult = await collection.update_one(
+            {"_id": user.id},
             {"$set": user_dumped},
         )
         if result.modified_count == 0:
-            raise HTTPException(
-                status_code=404, detail="User not found or no changes applied"
+            return UpdateResponse(
+                success=False, modified_count=0, detail="User not found"
             )
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+
+    except Exception:
+        return UpdateResponse(success=False, modified_count=0)
 
     return UpdateResponse(success=True, modified_count=result.modified_count)
 
