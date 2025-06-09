6aa8b2214445aa323831d08145b0892a0f04f7d5
rtk-rnjn <ritik0ranjan+github@gmail.com>
2025-06-09 11:34:19 +0530

    fix: update OTP handling to return False for non-existent users and remove token verification check

--- src/routes/otp.py
+++ src/routes/otp.py
@@ -20,7 +20,7 @@ def get_user_token(credentials: HTTPAuthorizationCredentials = Depends(security)
 async def send_otp(request: Request, email_address: str):
     _user = await cache_handler.user_exists(email_address=email_address)
     if not _user:
-        raise HTTPException(status_code=404, detail="User not found")
+        return False
 
     otp = await cache_handler.generate_otp(email_address=email_address)
 
@@ -29,17 +29,18 @@ async def send_otp(request: Request, email_address: str):
         otp=otp,
     )
 
+    return True
+
 
 @router.post("/verify")
 async def verify_otp(request: Request, email_address: str, otp: str, token: Token = Depends(get_user_token)):
     _user = await cache_handler.user_exists(email_address=email_address)
     if not _user:
-        raise HTTPException(status_code=404, detail="User not found")
+        return False
 
     if not await cache_handler.verify_otp(email_address=email_address, otp=otp):
         return False
 
-    # Update the user's last login time
     await cache_handler.update_user(user_id=token.sub, updated_user=BaseModel(is_verified=True))
 
     return True
